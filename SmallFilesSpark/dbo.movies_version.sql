e97c36a31b4ba8c02a1d64e67b680e5629d79843
René Bremer <32130834+rebremer@users.noreply.github.com>
2022-10-10 08:54:17 +0200

    Create dbo.movies_version.sql

---
+++ SmallFilesSpark/dbo.movies_version.sql
@@ -0,0 +1,35 @@
+ines (29 sloc) 2.51 KB
+/****** Object:  Table [dbo].[movies_int]    Script Date: 9/27/2022 8:54:12 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[movies_version]
+
+CREATE TABLE [dbo].[movies_version](
+	[movie] [int] NULL,
+	[version] [int] NULL,
+	[update_time] [date] NULL,
+	[title] [nvarchar](max) NULL,
+	[genres] [nvarchar](max) NULL,
+	[year] [nvarchar](max) NULL,
+	[Rating] [nvarchar](max) NULL,
+	[Rotten_tomato] [nvarchar](max) NULL,
+	[description] [nvarchar](max)
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+INSERT INTO [dbo].[movies_version]
+SELECT [movie], 
+	  (ROW_NUMBER() OVER (PARTITION By movie ORDER BY movie)  -1 ) as versionrbr
+      ,  DATEADD(day,  (ROW_NUMBER() OVER (PARTITION By movie ORDER BY movie) - 1), CAST( GETDATE() AS Date ))
+	  ,[title]
+      ,[genres]
+      ,[year]
+      ,ABS(CHECKSUM(NewId())) % 101
+      ,ABS(CHECKSUM(NewId())) % 101
+	  , CONVERT(nvarchar(max), CONCAT(NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID(),NEWID(), NEWID()))
+  FROM [dbo].[movies]
