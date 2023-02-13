3acb9b2894d8812ff99c70459cff7a9f0df9a887
René Bremer <32130834+rebremer@users.noreply.github.com>
2023-02-13 13:45:40 +0100

    Update function_init.py
    Store AWS S3 temp tokens to Azure Key Vault

--- ADFv2/aws-openid-connect/function_init.py
+++ ADFv2/aws-openid-connect/function_init.py
@@ -1,19 +1,23 @@
 import logging
 import boto3
 
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
-    # bearer token passed by ADF webactivity using system assigned MI and resource of app reg that is used as identity provider in AWS
-    bearer = req.headers['Authorization'][7:] 
+    logging.info('Python HTTP trigger function processed a request.')
+    keyVaultName = "<<your azure key vault name>>"
+    bearer = req.headers['Authorization'][7:]
     sts_client = boto3.client('sts')
 
     # Call the assume_role method of the STSConnection object and pass the role
     # ARN and a role session name.
     response = sts_client.assume_role_with_web_identity(
-        RoleArn='arn:aws:iam::<<your aws account>>:role/AzureADWebIdentity3',
+        RoleArn='arn:aws:iam::<<your aws account id>>:role/AzureADWebIdentity3',
         RoleSessionName='app1',
         WebIdentityToken=bearer,
         DurationSeconds=3600
@@ -21,23 +25,36 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # From the response that contains the assumed role, get the temporary 
     # credentials that can be used to make subsequent API calls
-    credentials=response['Credentials']
+    aws_temp_credentials=response['Credentials']
 
+    KVUri = f"https://{keyVaultName}.vault.azure.net"
+
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=KVUri, credential=credential)
+
+    logging.info('Write temp AWS keys to key vault')
+    client.set_secret("adf-test-aws-aws-accesskeyid", aws_temp_credentials['AccessKeyId'])
+    client.set_secret("adf-test-aws-aws-secretaccesskey", aws_temp_credentials['SecretAccessKey'])
+    client.set_secret("adf-test-aws-aws-sessiontoken", aws_temp_credentials['SessionToken'])
+
+    return func.HttpResponse(f"temp AWS keys succesfully writen to key vault")
+
+    #
+    # Alternative if you want to fetch AWS S3 direclty via Azure Function
+    #
     # Use the temporary credentials that AssumeRole returns to make a 
     # connection to Amazon S3  
-    s3_resource=boto3.resource(
-        's3',
-        aws_access_key_id=credentials['AccessKeyId'],
-        aws_secret_access_key=credentials['SecretAccessKey'],
-        aws_session_token=credentials['SessionToken'],
-    )
+    # s3_resource=boto3.resource(
+    #    's3',
+    #    aws_access_key_id=credentials['AccessKeyId'],
+    #    aws_secret_access_key=credentials['SecretAccessKey'],
+    #    aws_session_token=credentials['SessionToken'],
+    #)
 
     # Use the Amazon S3 resource object that is now configured with the 
     # credentials to access your S3 buckets. 
-    for bucket in s3_resource.buckets.all():
-        print(bucket.name)
-        logging.info('Python HTTP trigger function processed a request.')
+    # for bucket in s3_resource.buckets.all():
+    ##   print(bucket.name)
+    #    last_bucket = bucket.name
 
-    last_bucket = bucket.name
-
-    return func.HttpResponse(f"Last bucket in AWS: {last_bucket}.")
+    # return func.HttpResponse(f"Last bucket in AWS: {last_bucket}.")
